d732bfff658c2d7e3e27b85f0a1783a2c2d6b7e9
yael2yoch <36213876832@mby.co.il>
2023-12-19 16:58:30 +0200

    api connevtion and test

--- wishes/app.js
+++ wishes/app.js
@@ -3,50 +3,40 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const OpenAIAPI = require('openai');
-const openai = new OpenAIAPI();
+const openai = new OpenAIAPI({ apiKey:process.env.OPENAI_API_KEY })
 
 const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs');
+const runPrompt = async (req,res) => {
+    const userInput = req.body;
+    const prompt =
+        `hi, please write me  a ${userInput.mood} ${userInput.wishType} for ${userInput.person}'s ${userInput.event},
+        return parsable JSON format '
+        `;
+    //console.log("prompt is:",prompt);
+
+    const response = await openai.completions.create({
+        model: 'text-davinci-003',
+        prompt: prompt,
+        max_tokens: 400,
+        n: 3 
+        });
+    const opt1 = response.choices[0].text;
+    const opt2 = response.choices[1].text;
+    const opt3 = response.choices[2].text;
+
+    res.send({"opt1":opt1,
+              "opt2":opt2,
+              "opt3":opt3})
+
+
+    }
 
 app.post('/prompts', (req, res) => {
 
-    const runPrompt = async () => {
-        const userInput = req.body['options'];
-        const prompt =
-            f`hi, please write me 3 options for a
-        ${userInput.mood} ${userInput.wishType} 
-        for ${userInput.person}'s ${userInput.event},
-        `;
+    runPrompt(req,res)
 
-
-        const response = await openai.createCompletion({
-            
-            model: "gpt-3.5-turbo",
-            prompt: prompt,
-            maxToken: 400
-        })
-        debugger;
-        const parsableResponse = response.data.choices[0];
-        console.log('res test:', parsableResponse);
-        let parsedResponse;
-        try {
-            parsedResponse = JSON.parse(parsableResponse)
-        }
-        catch (error) {
-            console.error('error: parsing JSON response: ', error)
-            return {};
-        }
-
-        return { parsedResponse };
-
-    }
-    runPrompt().then(({ parsedResponse }) => {
-        if (Object.keys(parsedResponse).length > 0)
-            console.log("parse respone", parsedResponse);
-        else
-        console.log("i dont have response");
-    })
 })
 app.listen(8989, (req, res) => {
     console.log('app running')
